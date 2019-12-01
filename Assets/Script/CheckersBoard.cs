26b7aecc31843532b85fbda14a3b61551f8cdc25
Puf Vlad <39412898+VladPuf@users.noreply.github.com>
2019-12-01 17:25:12 +0200

    Update v3

--- Assets/Script/CheckersBoard.cs
+++ Assets/Script/CheckersBoard.cs
@@ -13,6 +13,9 @@ public class CheckersBoard : MonoBehaviour
     private Vector3 boardOffset = new Vector3(-4.0f, 0, -4.0f);
     private Vector3 pieceOffset = new Vector3(0.5f, 0, 0.5f);
 
+    private bool isWhite;
+    private bool isWhiteTurn;
+
     private Vector2 mouseOver;
     private Vector2 startDrag;
     private Vector2 endDrag;
@@ -22,6 +25,7 @@ public class CheckersBoard : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        isWhiteTurn = true;
         GenerateBoard();
     }
 
@@ -34,6 +38,9 @@ public class CheckersBoard : MonoBehaviour
         {
             int x = (int)mouseOver.x;
             int y = (int)mouseOver.y;
+
+            if (selectedPiece != null)
+                UpdatePieceDrag(selectedPiece);
 
             if (Input.GetMouseButtonDown(1))
             {
@@ -70,6 +77,21 @@ public class CheckersBoard : MonoBehaviour
         }
     }
 
+    private void UpdatePieceDrag(Piece p)
+    {
+        if(!Camera.main)
+        {
+            Debug.Log("Unable to find main camera");
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("Board")))
+        {
+            p.transform.position = hit.point + Vector3.up;
+        }
+    }
+
     private void SelectPiece(int x, int y)
     {
         //out of bounds
@@ -85,10 +107,59 @@ public class CheckersBoard : MonoBehaviour
     }
     private void TryMove(int x1, int y1, int x2, int y2)
     {
+        // Multimpayer Support
         startDrag = new Vector2(x1, y1);
         endDrag = new Vector2(x2, y2);
-
         selectedPiece = pieces[x1, y1];
+
+        // Out of bounds
+        if (x2 < 0 || x2 >= pieces.Length || y2 < 0 || y2 >= pieces.Length)
+        {
+            if (selectedPiece != null)
+                MovePiece(selectedPiece, x1, y1);
+
+            startDrag = Vector2.zero;
+            selectedPiece = null;
+            return;
+        }
+
+        if(selectedPiece != null)
+        {
+            // If it has not moved
+            if(endDrag == startDrag)
+            {
+                MovePiece(selectedPiece, x1, y1);
+                startDrag = Vector2.zero;
+                selectedPiece = null;
+                return;
+            }
+
+            // Check if its a valid move
+            if(selectedPiece.ValidMove(pieces,x1,y1,x2,y2))
+            {
+                // Did we kill anything
+                // If this is a jump
+                if(Mathf.Abs(x2-x2) == 2)
+                {
+                    Piece p = pieces[(x1 + x2) / 2, (y1 + y2) / 2];
+
+                    if(p!= null)
+                    {
+                        pieces[(x1 + x2) / 2, (y1 + y2) / 2] = null;
+                        Destroy(p);
+                    }
+                }
+
+                pieces[x2, y2] = selectedPiece;
+                pieces[x1, y1] = null;
+
+                MovePiece(selectedPiece, x2, y2);
+
+                EndTurn();
+            }
+        }
+
+        /*
         //check distance max 1 (to be removed after jump over pieces is implemented)
         if ((Math.Abs(x2 - x1) > 1) || (Math.Abs(y2 - y1) > 1))
             return;
@@ -107,7 +178,22 @@ public class CheckersBoard : MonoBehaviour
         //if Valid move
         MovePiece(selectedPiece, x2, y2);
         pieces[x1, y1] = null;
-        pieces[x2, y2] = selectedPiece;
+        pieces[x2, y2] = selectedPiece;*/
+    }
+
+    private void EndTurn()
+    {
+        selectedPiece = null;
+        startDrag = Vector2.zero;
+
+        isWhiteTurn = !isWhiteTurn;
+
+        CheckVictory();
+    }
+
+    private void CheckVictory()
+    {
+
     }
 
     private void GenerateBoard()
--- Assets/Script/Piece.cs
+++ Assets/Script/Piece.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool isWhite;
+    public bool isKing;
 
-    // Update is called once per frame
-    void Update()
+    public bool ValidMove(Piece[,] board, int x1, int y1, int x2, int y2)
     {
-        
+        //If you are moving on top of another piece
+        if (board[x2, y2] != null)
+            return false;
+
+        int deltaMove = Mathf.Abs(x1 - x2);
+        int deltaMoveY = y2 - y1;
+
+        // For white
+        if (isWhite || isKing)
+        {
+                       
+            if(deltaMove == 1)
+            {
+                if (deltaMoveY == 1)
+                    return true;
+            }
+            else if(deltaMove == 2)
+            {
+                if (deltaMoveY == 2)
+                {
+                    Piece p = board[(x1 + x2) / 2, (y1 + y2) / 2];
+
+                    if (p != null && p.isWhite != isWhite)
+                        return true;
+                }                                      
+            }
+        }
+
+        // For black
+        if (!isWhite || isKing)
+        {
+                       
+            if(deltaMove == 1)
+            {
+                if (deltaMoveY == -1)
+                    return true;
+            }
+            else if(deltaMove == 2)
+            {
+                if (deltaMoveY == -2)
+                {
+                    Piece p = board[(x1 + x2) / 2, (y1 + y2) / 2];
+
+                    if (p != null && p.isWhite != isWhite)
+                        return true;
+                }                                      
+            }
+        }
+
+        return false;
     }
 }
